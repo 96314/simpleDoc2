bce09a57a7f1694f95a08d0405656917716caade
刘聪 <rainburst@yeah.net>
2020-12-26 19:14:51 +0800

    完整运营的版本

---
+++ src/main/java/com/github/apiggs/example/common/UserQuery.java
@@ -0,0 +1,16 @@
+package com.github.apiggs.example.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserQuery extends Page {
+
+    /**
+     * 名称
+     * @value apiggs
+     */
+    String name;
+
+}
